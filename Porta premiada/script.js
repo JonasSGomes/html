649fb8649fa38aff61286c592d501b3846c6fc54
JonasSGomes <jonassgsantos11@gmail.com>
2026-04-16 15:37:37 -0300

    Add files via upload

---
+++ Porta premiada/script.js
@@ -0,0 +1,32 @@
+let imagem = document.getElementById("imagem")
+let imagem2 = document.getElementById("imagem2")
+let imagem3 = document.getElementById("imagem3")
+
+
+imagem.addEventListener("mouseover", function(){
+    imagem.src = "https://png.pngtree.com/png-vector/20240717/ourmid/pngtree-kitchen-trash-bag-png-image_13134176.png"
+})
+
+
+imagem.addEventListener("mouseout", function(){
+    imagem.src = "https://png.pngtree.com/png-clipart/20220731/ourmid/pngtree-wooden-door-png-image_6093732.png"
+})
+
+imagem2.addEventListener("mouseover", function(){
+    imagem2.src = "https://png.pngtree.com/png-vector/20240717/ourmid/pngtree-kitchen-trash-bag-png-image_13134176.png"
+})
+
+
+imagem2.addEventListener("mouseout", function(){
+    imagem2.src = "https://png.pngtree.com/png-clipart/20220731/ourmid/pngtree-wooden-door-png-image_6093732.png"
+})
+
+
+imagem3.addEventListener("mouseover", function(){
+    imagem3.src = "https://png.pngtree.com/png-vector/20240528/ourmid/pngtree-a-bag-of-gold-png-image_12505724.png"
+})
+
+
+imagem3.addEventListener("mouseout", function(){
+    imagem3.src = "https://png.pngtree.com/png-clipart/20220731/ourmid/pngtree-wooden-door-png-image_6093732.png"
+})
